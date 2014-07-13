80a508b2006eae242f4df2413382c8ad13d67ae5
Matt Whitesides <mbwxd4@mail.missouri.edu>
2014-07-12 23:21:07 -0500

    Fixed stuff I messed up hopefully..

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,28 +1,105 @@
-# Assignment:
-number   = 42
-opposite = true
+# detect touch 
 
-# Conditions:
-number = -42 if opposite
+# background fix 
 
-# Functions:
-square = (x) -> x * x
+# fix vertical when not overflow
+#call fullscreenFix() if .fullscreen content changes 
+fullscreenFix = ->
+  h = $("body").height()
+  # set .fullscreen height
+  $(".content-b").each (i) ->
+    $(this).closest(".fullscreen").addClass "not-overflow"  if $(this).innerHeight() <= h
+    return
+  return
 
-# Arrays:
-list = [1, 2, 3, 4, 5]
+# resize background images 
+backgroundResize = ->
+  windowH = $(window).height()
+  $(".background").each (i) ->
+    path = $(this)
+    
+    # variables
+    contW = path.width()
+    contH = path.height()
+    imgW = path.attr("data-img-width")
+    imgH = path.attr("data-img-height")
+    ratio = imgW / imgH
+    
+    # overflowing difference
+    diff = parseFloat(path.attr("data-diff"))
+    diff = (if diff then diff else 0)
+    
+    # remaining height to have fullscreen image only on parallax
+    remainingH = 0
+    if path.hasClass("parallax") and not $("html").hasClass("touch")
+      maxH = (if contH > windowH then contH else windowH)
+      remainingH = windowH - contH
+    
+    # set img values depending on cont
+    imgH = contH + remainingH + diff
+    imgW = imgH * ratio
+    
+    # fix when too large
+    if contW > imgW
+      imgW = contW
+      imgH = imgW / ratio
+    
+    #
+    path.data "resized-imgW", imgW
+    path.data "resized-imgH", imgH
+    path.css "background-size", imgW + "px " + imgH + "px"
+    return
 
-# Objects:
-math =
-  root:   Math.sqrt
-  square: square
-  cube:   (x) -> x * square x
+  return
 
-# Splats:
-race = (winner, runners...) ->
-  print winner, runners
+# set parallax background-position 
+parallaxPosition = (e) ->
+  heightWindow = $(window).height()
+  topWindow = $(window).scrollTop()
+  bottomWindow = topWindow + heightWindow
+  currentWindow = (topWindow + bottomWindow) / 2
+  $(".parallax").each (i) ->
+    path = $(this)
+    height = path.height()
+    top = path.offset().top
+    bottom = top + height
+    
+    # only when in range
+    if bottomWindow > top and topWindow < bottom
+      imgW = path.data("resized-imgW")
+      imgH = path.data("resized-imgH")
+      
+      # min when image touch top of window
+      min = 0
+      
+      # max when image touch bottom of window
+      max = -imgH + heightWindow
+      
+      # overflow changes parallax
+      overflowH = (if height < heightWindow then imgH - height else imgH - heightWindow) # fix height on overflow
+      top = top - overflowH
+      bottom = bottom + overflowH
+      
+      # value with linear interpolation
+      value = min + (max - min) * (currentWindow - top) / (bottom - top)
+      
+      # set background-position
+      orizontalPosition = path.attr("data-oriz-pos")
+      orizontalPosition = (if orizontalPosition then orizontalPosition else "50%")
+      $(this).css "background-position", orizontalPosition + " " + value + "px"
+    return
 
-# Existence:
-alert "I knew it!" if elvis?
-
-# Array comprehensions:
-cubes = (math.cube num for num in list)
+  return
+document.documentElement.className = document.documentElement.className + " touch"  if "ontouchstart" of window
+$(".parallax").css "background-attachment", "fixed"  unless $("html").hasClass("touch")
+$(window).resize fullscreenFix
+fullscreenFix()
+$(window).resize backgroundResize
+$(window).focus backgroundResize
+backgroundResize()
+unless $("html").hasClass("touch")
+  $(window).resize parallaxPosition
+  
+  #$(window).focus(parallaxPosition);
+  $(window).scroll parallaxPosition
+  parallaxPosition()
--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,89 +1,102 @@
-/* fix vertical when not overflow call fullscreenFix() if .fullscreen content changes */
-function fullscreenFix(){
-    var h = $('body').height();
-    // set .fullscreen height
-    $(".content-b").each(function(i){
-        if($(this).innerHeight() <= h){
-            $(this).closest(".fullscreen").addClass("not-overflow");
-        }
-    });
-}
-$(window).resize(fullscreenFix);
-fullscreenFix();
+(function() {
 
-/* resize background images */
-function backgroundResize(){
-    var windowH = $(window).height();
-    $(".background").each(function(i){
-        var path = $(this);
-        // variables
-        var contW = path.width();
-        var contH = path.height();
-        var imgW = path.attr("data-img-width");
-        var imgH = path.attr("data-img-height");
-        var ratio = imgW / imgH;
-        // overflowing difference
-        var diff = parseFloat(path.attr("data-diff"));
-        diff = diff ? diff : 0;
-        // remaining height to have fullscreen image only on parallax
-        var remainingH = 0;
-        if(path.hasClass("parallax")){
-            var maxH = contH > windowH ? contH : windowH;
-            remainingH = windowH - contH;
-        }
-        // set img values depending on cont
-        imgH = contH + remainingH + diff;
-        imgW = imgH * ratio;
-        // fix when too large
-        if(contW > imgW){
-            imgW = contW;
-            imgH = imgW / ratio;
-        }
-        //
-        path.data("resized-imgW", imgW);
-        path.data("resized-imgH", imgH);
-        path.css("background-size", imgW + "px " + imgH + "px");
-    });
-}
-$(window).resize(backgroundResize);
-$(window).focus(backgroundResize);
-backgroundResize();
 
-/* set parallax background-position */
-function parallaxPosition(e){
-    var heightWindow = $(window).height();
-    var topWindow = $(window).scrollTop();
-    var bottomWindow = topWindow + heightWindow;
-    var currentWindow = (topWindow + bottomWindow) / 2;
-    $(".parallax").each(function(i){
-    var path = $(this);
-        var height = path.height();
-        var top = path.offset().top;
-        var bottom = top + height;
-        // only when in range
-        if(bottomWindow > top && topWindow < bottom){
-            var imgW = path.data("resized-imgW");
-            var imgH = path.data("resized-imgH");
-            // min when image touch top of window
-            var min = 0;
-            // max when image touch bottom of window
-            var max = - imgH + heightWindow;
-            // overflow changes parallax
-            var overflowH = height < heightWindow ? imgH - height : imgH - heightWindow; // fix height on overflow
-            top = top - overflowH;
-            bottom = bottom + overflowH;
-            // value with linear interpolation
-            var value = min + (max - min) * (currentWindow - top) / (bottom - top);
-            // set background-position
-            var orizontalPosition = path.attr("data-oriz-pos");
-            orizontalPosition = orizontalPosition ? orizontalPosition : "50%";
-            $(this).css("background-position", orizontalPosition + " " + value + "px");
-        }
+
+}).call(this);
+
+(function() {
+  var backgroundResize, fullscreenFix, parallaxPosition;
+
+  fullscreenFix = function() {
+    var h;
+    h = $("body").height();
+    $(".content-b").each(function(i) {
+      if ($(this).innerHeight() <= h) {
+        $(this).closest(".fullscreen").addClass("not-overflow");
+      }
     });
-}
-if(!$("html").hasClass("touch")){
+  };
+
+  backgroundResize = function() {
+    var windowH;
+    windowH = $(window).height();
+    $(".background").each(function(i) {
+      var contH, contW, diff, imgH, imgW, maxH, path, ratio, remainingH;
+      path = $(this);
+      contW = path.width();
+      contH = path.height();
+      imgW = path.attr("data-img-width");
+      imgH = path.attr("data-img-height");
+      ratio = imgW / imgH;
+      diff = parseFloat(path.attr("data-diff"));
+      diff = (diff ? diff : 0);
+      remainingH = 0;
+      if (path.hasClass("parallax") && !$("html").hasClass("touch")) {
+        maxH = (contH > windowH ? contH : windowH);
+        remainingH = windowH - contH;
+      }
+      imgH = contH + remainingH + diff;
+      imgW = imgH * ratio;
+      if (contW > imgW) {
+        imgW = contW;
+        imgH = imgW / ratio;
+      }
+      path.data("resized-imgW", imgW);
+      path.data("resized-imgH", imgH);
+      path.css("background-size", imgW + "px " + imgH + "px");
+    });
+  };
+
+  parallaxPosition = function(e) {
+    var bottomWindow, currentWindow, heightWindow, topWindow;
+    heightWindow = $(window).height();
+    topWindow = $(window).scrollTop();
+    bottomWindow = topWindow + heightWindow;
+    currentWindow = (topWindow + bottomWindow) / 2;
+    $(".parallax").each(function(i) {
+      var bottom, height, imgH, imgW, max, min, orizontalPosition, overflowH, path, top, value;
+      path = $(this);
+      height = path.height();
+      top = path.offset().top;
+      bottom = top + height;
+      if (bottomWindow > top && topWindow < bottom) {
+        imgW = path.data("resized-imgW");
+        imgH = path.data("resized-imgH");
+        min = 0;
+        max = -imgH + heightWindow;
+        overflowH = (height < heightWindow ? imgH - height : imgH - heightWindow);
+        top = top - overflowH;
+        bottom = bottom + overflowH;
+        value = min + (max - min) * (currentWindow - top) / (bottom - top);
+        orizontalPosition = path.attr("data-oriz-pos");
+        orizontalPosition = (orizontalPosition ? orizontalPosition : "50%");
+        $(this).css("background-position", orizontalPosition + " " + value + "px");
+      }
+    });
+  };
+
+  if ("ontouchstart" in window) {
+    document.documentElement.className = document.documentElement.className + " touch";
+  }
+
+  if (!$("html").hasClass("touch")) {
+    $(".parallax").css("background-attachment", "fixed");
+  }
+
+  $(window).resize(fullscreenFix);
+
+  fullscreenFix();
+
+  $(window).resize(backgroundResize);
+
+  $(window).focus(backgroundResize);
+
+  backgroundResize();
+
+  if (!$("html").hasClass("touch")) {
     $(window).resize(parallaxPosition);
-    //$(window).focus(parallaxPosition);
     $(window).scroll(parallaxPosition);
     parallaxPosition();
-}
+  }
+
+}).call(this);
